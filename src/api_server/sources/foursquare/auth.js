1d0eaa3242d1a1ff43340285ff781393776f47e9
Timo Sand <timo.sand@iki.fi>
2017-12-19 00:26:41 +0200

    Removes unused deauth

--- src/api_server/sources/foursquare/auth.js
+++ src/api_server/sources/foursquare/auth.js
@@ -5,16 +5,13 @@
  */
 
 
-const { FOURSQUARE_API_TIMEOUT } = require('./base');
-const { deauth_source, save_source, send_home_redirects } = require('../source');
-const { logger, rp } = require('../../config');
+const { deauth_source, ensured_source_exists, save_source, send_home_redirects } = require('../source');
 const secrets = require('../../secrets');
-const { ensured_logged_in, get_callback_url, get_static_url, split_source_id } = require('../../utils');
+const { ensured_logged_in, get_callback_url, get_static_url } = require('../../utils');
 
 const express = require('express');
 const passport = require('passport');
 const FoursquareStrategy = require('passport-foursquare').Strategy;
-const _ = require('lodash');
 
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -49,41 +46,14 @@ router.get(
 router.get(
     '/deauth/:source_id*',
     ensured_logged_in,
+    ensured_source_exists("source_id"),
     (req, res, next) => {
-        const source_id = req.params.source_id;
-        const user = req.user;
-
-        const source = user.get_source(source_id);
-        if (_.isUndefined(source)) {
-            res.status(404).json({
-                msg: `bad source id: \`${source_id}\``,
-            });
-            next();
-        } else {
-            const { provider_user_id } = split_source_id(source_id);
-            const options = {
-                method: 'POST',
-                uri: 'http://www.meetup.com/api/?method=grantOauthAccess',
-                qs: {
-                    method: 'grantOauthAccess',
-                },
-                form: {
-                    arg_clientId: secrets.get('FOURSQUARE_CLIENT_INTERNAL_ID'),
-                    arg_member: provider_user_id,
-                    arg_grant: false,
-                },
-                json: true,
-                timeout: FOURSQUARE_API_TIMEOUT,
-            };
-            rp(options)
-                .then(() => {
-                    logger.debug('%s revoked source `%s` for user `%s`',
-                                 req.id, source_id, user.id);
-                })
-                .catch(next);
-            deauth_source(req, source);
-            next();
-        }
+        // TODO: need to ask the user to go to Foursquare to revoke the app
+        // NOTE: the `then` is here to make sure we're not passing anything
+        // to express `next` as it would be interpreted as an error.
+        deauth_source(req, req.user.get_source(req.params.source_id))
+            .then(() => next())
+            .catch(next);
     },
     send_home_redirects
 );
